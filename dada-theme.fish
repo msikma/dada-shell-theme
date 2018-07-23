7a5b06c0d6a95cf738b32df06b2c4489d0ff05a4
Michiel Sikma <michiel@sikma.org>
2018-07-23 22:55:58 +0200

    Better Git handling

--- dada-theme.fish
+++ dada-theme.fish
@@ -5,6 +5,21 @@ function fish_greeting
   set_color normal
 end
 
+function faster_vcs
+  # Speed up non-Git folders a bit
+  if not test -d ./.git; and not test -d ../.git
+    return
+  end
+  __fish_vcs_prompt
+end
+
+set -g __fish_git_prompt_showdirtystate 1
+set -g __fish_git_prompt_showstashstate 1
+set -g __fish_git_prompt_showuntrackedfiles 1
+set -g __fish_git_prompt_describe_style 'branch'
+#set -g __fish_git_prompt_showcolorhints 1
+set -g __fish_git_prompt_show_informative_status 1
+
 # Copied from one of the default prompts and edited a bit.
 function fish_prompt --description 'Write out the prompt'
   if not set -q __fish_prompt_normal
@@ -15,5 +30,5 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_prompt_cwd (set_color $fish_color_cwd)
   end
 
-  echo -n -s "$__fish_prompt_cwd" (prompt_pwd) (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
+  echo -n -s "$__fish_prompt_cwd" (prompt_pwd) (faster_vcs) "$__fish_prompt_normal" '> '
 end
