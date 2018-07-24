c9e0692f7459fe1d1d8e0760e1dbea4868642205
Michiel Sikma <michiel@sikma.org>
2018-07-24 21:54:16 +0200

    Add cool stuff to the prompt, a date to the right prompt, and a basic greeting

--- dada-theme.fish
+++ dada-theme.fish
@@ -1,7 +1,62 @@
+set -g __fish_git_prompt_showdirtystate 1
+set -g __fish_git_prompt_showstashstate 1
+set -g __fish_git_prompt_showuntrackedfiles 1
+set -g __fish_git_prompt_describe_style 'branch'
+set -g __fish_git_prompt_showcolorhints 1
+set -g __fish_git_prompt_show_informative_status 1
+
+set -g __fish_git_prompt_char_stateseparator ' '
+set -g __fish_git_prompt_char_dirtystate '+'
+set -g __fish_git_prompt_char_invalidstate '×'
+set -g __fish_git_prompt_char_untrackedfiles '…'
+set -g __fish_git_prompt_char_stagedstate '*'
+set -g __fish_git_prompt_char_cleanstate '✓'
+
+set -g __fish_git_prompt_color_prefix yellow
+set -g __fish_git_prompt_color_suffix yellow
+set -g __fish_git_prompt_color_branch yellow
+
+# CWD color
+set -g __fish_prompt_cwd (set_color cyan)
+set -g __fish_prompt_normal (set_color normal)
+
+# Copied from one of the default prompts and edited a bit.
+function fish_prompt --description 'Write out the prompt'
+  echo -n -s "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" (faster_vcs) "$__fish_prompt_normal" '> '
+end
+
+function fish_right_prompt --description 'Write out the right prompt'
+  set -l exit_code $status
+  set -l datestr (date +"%a, %b %d %Y %H:%m:%d")
+  if test $exit_code -ne 0
+    set_color red
+    echo -n "! "
+    set_color normal
+  end
+  
+  set_color 222
+  echo $datestr
+  set_color normal
+end
+
 function fish_greeting
+  # Disk usage in %
+  set disk_usage_perc (df / | tail -n1 | sed "s/  */ /g" | cut -d' ' -f 5 | cut -d'%' -f1)
+  # Disk usage in GB, one decimal
+  set disk_usage_gb (math --scale=1 "("(df / | tail -n1 | sed "s/  */ /g" | cut -d' ' -f 4)"*512)/1000000000")
+  # Total disk size in GB, one decimal
+  set disk_total_gb (math --scale=1 "("(df / | tail -n1 | sed "s/  */ /g" | cut -d' ' -f 2)"*512)/1000000000")
+  # user@hostname
+  set user (whoami)'@'(uname -n)
+  # Current IP (10.0.1.3)
+  set currip (ifconfig | grep inet | grep broadcast | cut -d' ' -f 2)
+  
   set_color brblack
-  echo (uname -n)  (uname -srmp)
+  echo (uname -v)
   uptime
+  echo
+  echo -s User: "       " $user " (" $currip ")"
+  echo Disk usage: $disk_usage_perc% "("$disk_usage_gb GB available")"
   set_color normal
 end
 
@@ -11,24 +66,4 @@ function faster_vcs
     return
   end
   __fish_vcs_prompt
-end
-
-set -g __fish_git_prompt_showdirtystate 1
-set -g __fish_git_prompt_showstashstate 1
-set -g __fish_git_prompt_showuntrackedfiles 1
-set -g __fish_git_prompt_describe_style 'branch'
-#set -g __fish_git_prompt_showcolorhints 1
-set -g __fish_git_prompt_show_informative_status 1
-
-# Copied from one of the default prompts and edited a bit.
-function fish_prompt --description 'Write out the prompt'
-  if not set -q __fish_prompt_normal
-    set -g __fish_prompt_normal (set_color normal)
-  end
-
-  if not set -q __fish_prompt_cwd
-    set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-  end
-
-  echo -n -s "$__fish_prompt_cwd" (prompt_pwd) (faster_vcs) "$__fish_prompt_normal" '> '
 end
