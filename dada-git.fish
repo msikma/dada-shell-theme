b1e8bffefe86baef204f9988f56f02b44d4eea5e
Michiel Sikma <michiel@sikma.org>
2018-07-30 13:38:18 +0200

    Display more branches

--- dada-git.fish
+++ dada-git.fish
@@ -4,7 +4,7 @@ function gc
   echo "gl      - git log with merge lines"
   echo "gsl     - git short log (one liners)"
 end
-alias gb="git for-each-ref --count=12 --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
+alias gb="git for-each-ref --count=25 --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 alias gits="git status"
 alias gl="git log --pretty --oneline --graph"
 alias gsl="git log --oneline --decorate --color | head"
