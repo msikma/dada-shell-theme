0db8854e4a4f67d459c80b0ea7665e7782d828b5
Michiel Sikma <michiel@sikma.org>
2018-08-24 10:16:34 +0200

    Add gdc command

--- dada-git.fish
+++ dada-git.fish
@@ -5,10 +5,12 @@ function gc
                $c2"gits/g$c1        Git status"\
                $c2"gl$c1            Git log with merge lines"\
                $c2"gsl$c1           Git short log (one liners)"\
+               $c2"gdc$c1           Runs 'git diff --cached'"\
                $c2"git summary$c1   Summary of repo and authors"
 end
 alias gb="git for-each-ref --count=25 --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 alias gits="git status"
 alias g="git status"
+alias gdc="git diff --cached"
 alias gl="git log --pretty --oneline --graph"
 alias gsl="git log --oneline --decorate --color | head"
