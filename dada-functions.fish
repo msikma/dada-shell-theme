5f426e5628e1c3b7c83a84ebec37566062c20226
Michiel Sikma <michiel@sikma.org>
2018-08-28 00:04:32 +0200

    Fix indentation of command help

--- dada-functions.fish
+++ dada-functions.fish
@@ -24,39 +24,39 @@ function help
   echo "The following commands are available:"
   echo
   
-  set lines $c0"sphp         $c1 Changes PHP version"\
-            $c2"gits / g     $c1 Git status"\
-            $c0"devices      $c1 Displays local computers"\
-            $c2"gb           $c1 Last Git commits per branch"\
-            $c0"headers      $c1 Displays headers for a URL"\
-            $c2"gl           $c1 Git log with merge lines"\
-            $c0"scrapemp3s   $c1 Scrapes MP3 files from a URL"\
-            $c2"gsl          $c1 Git short log (one liners)"\
-            $c0"cdbackup     $c1 Changes directory to backup dir"\
-            $c2"gc           $c1 Displays Git commands help"\
-            $c0"fdupes       $c1 Finds duplicate files by hash"\
-            $c3"backup-music $c1 Backs up music"\
-            $c0"color        $c1 Adds a colored icon to a folder"\
-            $c3"backup-files $c1 Backs up ~/Files"\
-            $c0"empty-trash  $c1 Empties the trash bin"\
-            $c3"backup-zoo   $c1 Copies some music to Happy Zoo"\
-            $c0"tldr <cmd>   $c1 Displays simple command help"\
-            $c3"backup-src   $c1 Backs up source code dirs"\
-            $c0"tree         $c1 Runs ls with tree structure"\
-            $c3"backup-dbs   $c1 Backs up MySQL databases"\
+  set lines $c0"sphp           $c1 Changes PHP version"\
+            $c2"gits / g       $c1 Git status"\
+            $c0"devices        $c1 Displays local computers"\
+            $c2"gb             $c1 Last Git commits per branch"\
+            $c0"headers        $c1 Displays headers for a URL"\
+            $c2"gl             $c1 Git log with merge lines"\
+            $c0"scrapemp3s     $c1 Scrapes MP3 files from a URL"\
+            $c2"gsl            $c1 Git short log (one liners)"\
+            $c0"cdbackup       $c1 Changes directory to backup dir"\
+            $c2"gc             $c1 Displays Git commands help"\
+            $c0"fdupes         $c1 Finds duplicate files by hash"\
+            $c3"backup-music   $c1 Backs up music"\
+            $c0"color          $c1 Adds a colored icon to a folder"\
+            $c3"backup-files   $c1 Backs up ~/Files"\
+            $c0"empty-trash    $c1 Empties the trash bin"\
+            $c3"backup-zoo     $c1 Copies some music to Happy Zoo"\
+            $c0"tldr <cmd>     $c1 Displays simple command help"\
+            $c3"backup-src     $c1 Backs up source code dirs"\
+            $c0"tree           $c1 Runs ls with tree structure"\
+            $c3"backup-dbs     $c1 Backs up MySQL databases"\
   
   draw_columns $lines
   # Now the rest of the commands - all external applications.
   # Only things that are not part of Dada shell theme.
-  draw_columns $c4"youtube-dl$c1    Downloads videos from Youtube"\
-               $c4"streamlink$c1    Opens internet streams in VLC"\
-               $c4"ascr$c1          Downloads art from social media"\
-               $c4"weather$c1       Displays the current weather"\
-               $c4"bat$c1           Improved version of cat"\
-               $c4"glances$c1       Computer monitoring tool"\
-               $c4"bbedit <file>$c1 Opens a file in BBEdit"\
-               $c4"fd <str>$c1      Searches for files (alt. to find)"\
-               $c4"ncdu$c1          Displays disk space usage"
+  draw_columns $c4"youtube-dl$c1      Downloads videos from Youtube"\
+               $c4"streamlink$c1      Opens internet streams in VLC"\
+               $c4"ascr$c1            Downloads art from social media"\
+               $c4"weather$c1         Displays the current weather"\
+               $c4"bat$c1             Improved version of cat"\
+               $c4"glances$c1         Computer monitoring tool"\
+               $c4"bbedit <file>$c1   Opens a file in BBEdit"\
+               $c4"fd <str>$c1        Searches for files (alt. to find)"\
+               $c4"ncdu$c1            Displays disk space usage"
   echo
 end
 
--- dada-git.fish
+++ dada-git.fish
@@ -1,12 +1,12 @@
 function gc
   set c2 (set_color purple)
   set c1 (set_color white)
-  draw_columns $c2"gb$c1            Shows last commits per branch"\
-               $c2"gits/g$c1        Git status"\
-               $c2"gl$c1            Git log with merge lines"\
-               $c2"gsl$c1           Git short log (one liners)"\
-               $c2"gd$c1            Runs 'git diff --cached'"\
-               $c2"git summary$c1   Summary of repo and authors"
+  draw_columns $c2"gb$c1              Shows last commits per branch"\
+               $c2"gits/g$c1          Git status"\
+               $c2"gl$c1              Git log with merge lines"\
+               $c2"gsl$c1             Git short log (one liners)"\
+               $c2"gd$c1              Runs 'git diff --cached'"\
+               $c2"git summary$c1     Summary of repo and authors"
 end
 alias gb="git for-each-ref --count=25 --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 alias gits="git status"
