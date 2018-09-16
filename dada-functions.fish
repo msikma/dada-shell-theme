0a47e847f0c71925b019190d6547c265c5a29834
Michiel Sikma <michiel@sikma.org>
2018-09-16 02:10:07 +0200

    Correct variable check

--- dada-functions.fish
+++ dada-functions.fish
@@ -15,7 +15,7 @@ function update
   set old (get_version_short)
   set nvm (git pull)
   set new (get_version_short)
-  if [ old != new ]
+  if [ $old != $new ]
     echo "Updated Dada Shell Theme from "(set_color blue)$old(set_color normal)" to version "(set_color red)$new(set_color normal)"."
   else
     echo "You are already on the latest version of Dada Shell Theme, "(set_color red)$new(set_color normal)"."
