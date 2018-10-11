da73d2035250e91291aac55cea6ec34a35ea1b52
Michiel Sikma <michiel@sikma.org>
2018-10-12 01:27:21 +0200

    Add short log command for reading commit messages

--- dada-git.fish
+++ dada-git.fish
@@ -7,6 +7,7 @@ function gc
                $c2"gr$c1              Prints the repo remote URL"\
                $c2"gsl$c1             Git short log (one liners)"\
                $c2"gd$c1              Runs 'git diff --cached'"\
+               $c2"gs$c1              Shows list of commit messages"\
                $c2"git summary$c1     Summary of repo and authors"
 end
 alias gb="git for-each-ref --count=25 --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
@@ -16,3 +17,4 @@ alias gd="git diff --cached"
 alias gl="git log --pretty --oneline --graph"
 alias gsl="git log --oneline --decorate --color | head"
 alias gr="git remote get-url --all origin"
+alias gs="git log --pretty=oneline --abbrev-commit"
