6038188bfb8bdf171e6dc0113b08137bc18e7f02
Michiel Sikma <michiel@sikma.org>
2019-04-03 23:28:44 +0200

    Add cutvid to help function

--- dada-functions.fish
+++ dada-functions.fish
@@ -187,6 +187,7 @@ function help
             $c4"colortest$c1       Tests Terminal color settings"\
             $c4"ps2pdf <file>$c1   Convert PS to PDF; ps2pdf *.prn"\
             $c4"setssh$c1          Turns remote SSH access on/off"\
+            $c4"cutvid$c1          Handles common video editing tasks"\
             $c4"eatsql$c1          Shortcut to import SQL dumps"\
             $c4"urlredirs <url>$c1 Displays what redirects a URL has"\
 
