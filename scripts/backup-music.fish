45b5b561455c6ba4058d93d47f9894842fafb73e
Michiel Sikma <michiel@sikma.org>
2019-04-21 16:19:01 +0200

    New backup script format

--- scripts/backup-music.fish
+++ scripts/backup-music.fish
@@ -1,38 +1,21 @@
 #!/usr/bin/env fish
 
-timer_start
+set name "backup-music"
+set purpose "music"
 
-set err "backup-music: Error:"
-set last_backup (backup_time_rel "/Users/"(whoami)"/.cache/dada/backup-music")
 set src ~/"Files/Music/"
-set dst "/Volumes/Files/Backups/$dada_hostname/Files/Music/"
+set dst "/Volumes/Files/Music/Archive/"
 
-if not set -q dada_hostname
-  echo "$err \$dada_hostname is not set"
-  exit 1
-end
+check_rsync_version $name
+check_hostname $name
+check_needed_dirs $name 'source' $src
+check_needed_dirs $name 'target' $dst
 
-if not test -d $src
-  echo "$err Can't access source directory: $src"
-  exit 1
-end
+print_backup_start $purpose $name
+print_last_backup_time $name
+print_backup_dirs $src $dst
 
-if not test -d $dst
-  echo "$err Can't access target directory: $dst"
-  exit 1
-end
+copy_rsync $src $dst
 
-echo
-echo (set_color cyan)"Backup script for music"(set_color normal)
-echo (set_color green)"Copying to: "(set_color yellow)"$dst"(set_color normal)
-echo (set_color green)"Last backup was "$last_backup"."(set_color normal)
-echo
-
-rsync -ahvrESH8 --delete --progress --stats --exclude=".*/" --exclude=".*" $src $dst
-
-set result (timer_end)
-echo (set_color cyan)"Done in $result s."(set_color normal)
-echo
-
-mkdir -p ~/.cache/dada
-echo (date +"%a, %b %d %Y %X %z") > ~/.cache/dada/backup-music
+print_backup_finish $name
+set_last_backup $name
