5d88c7ad8d8052554f5fa237870f354d529813aa
Michiel Sikma <michiel@sikma.org>
2019-05-26 00:26:23 +0200

    Clean up Git commands, remove some and add one

--- functions/git.fish
+++ functions/git.fish
@@ -3,14 +3,15 @@
 set cmd_git \
   "ghelp"             "Displays all Git commands" \
   "g"                 "Git status" \
+  "gb"                "Shows last Git commits per branch" \
+  "gd"                "Runs 'git diff --cached'" \
   "gl"                "Git log with merge lines" \
+  "glb"               "Shows list of local-only branches" \
+  "glist"             "One-line Git log with merge lines" \
+  "glog"              "Git log with extra information" \
   "gr"                "Shows the repo's current remotes" \
   "gru"               "Shows the repo remote origin URL" \
   "gsl"               "Git short log (one liners)" \
-  "gd"                "Runs 'git diff --cached'" \
-  "gb"                "Shows last Git commits per branch" \
-  "gs"                "Shows list of commit messages" \
-  "glb"               "Shows list of local-only branches" \
 
 set cmd_git_scripts \
   "git summary"       "Summary of repo and authors" \
@@ -48,8 +49,10 @@ alias gith="ghelp" # legacy alias
 alias g="git status"
 alias gb="git for-each-ref --count=25 --sort=-committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 alias gd="git diff --cached"
-alias gl="git log --pretty --oneline --graph"
+alias gl="git log --name-status --graph"
+alias glist="git log --pretty --oneline --graph"
 alias gr="git remote -v"
 alias gru="git remote get-url --all origin"
-alias gs="git log --pretty=oneline --abbrev-commit"
 alias gsl="git log --oneline --decorate --color | head"
+alias glog='git log --graph --abbrev-commit --decorate --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(red) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)"'
+
