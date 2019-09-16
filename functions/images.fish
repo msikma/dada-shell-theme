f2759a1913f43608ef161247394474a9efceb391
Michiel Sikma <michiel@sikma.org>
2019-09-16 18:38:51 +0200

    Pass on all given arguments to the magick function

--- functions/images.fish
+++ functions/images.fish
@@ -1,11 +1,13 @@
 # Dada Shell Theme © 2019
 
 # Processing command for iSakura TV. Crops 22px off the top and bottom and converts to jpg.
-function img_isakura --argument-names file_arg
-  magick mogrify -format jpg -shave 0x22 "$file_arg"
+function img_isakura
+  set files $argv[1..-1]
+  magick mogrify -format jpg -shave 0x22 $files
 end
 
 # Used to cut off the edges from any image.
-function img_trim --argument-names file_arg
-  magick mogrify -trim "$file_arg"
+function img_trim
+  set files $argv[1..-1]
+  magick mogrify -trim $files
 end
