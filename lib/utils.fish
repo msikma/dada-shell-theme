8744808fbd59f0e35c0e0e6690ab0e47c49ce691
Michiel Sikma <michiel@sikma.org>
2019-11-17 01:18:52 +0100

    Add filters for converting text to lowercase/uppercase

--- lib/utils.fish
+++ lib/utils.fish
@@ -6,6 +6,12 @@ alias strip_color="gsed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g'"
 # Strips the extension from a filename
 alias strip_ext="sed -e 's/\.[^/]*\$//'"
 
+# Converts text to all lowercase
+alias lowercase="tr '[:upper:]' '[:lower:]'"
+
+# Converts text to all uppercase
+alias uppercase="tr '[:lower:]' '[:upper:]'"
+
 # lfext ./dir/ ".ext"
 # Lists all files of a certain extension inside a directory.
 # Doesn't break when there are zero matches.
