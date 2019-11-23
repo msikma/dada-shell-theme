9276e636f3ffc183c2648f8ea27ed90b399b0a13
Michiel Sikma <michiel@sikma.org>
2019-11-23 16:03:00 +0100

    Don't backup 'music' since the backup-music script is for that - and add ~/Library to the list of dirs to backup

--- scripts/backup-files.fish
+++ scripts/backup-files.fish
@@ -6,22 +6,25 @@ set purpose "misc. files"
 set src_files "$home/Files/"
 set src_desktop "$home/Desktop/"
 set src_documents "$home/Documents/"
+set src_library "$home/Library/"
 
 set dst_files "/Volumes/Files/Backups/$dada_hostname/Files"
 set dst_desktop "/Volumes/Files/Backups/$dada_hostname/Desktop"
 set dst_documents "/Volumes/Files/Backups/$dada_hostname/Documents"
+set dst_library "/Volumes/Files/Backups/$dada_hostname/Library"
 
 check_rsync_version $name
 check_hostname $name
-check_needed_dirs $name 'source' $src_files $src_desktop $src_documents
-check_needed_dirs $name 'target' $dst_files $dst_desktop $dst_documents
+check_needed_dirs $name 'source' $src_files $src_desktop $src_documents $src_library
+check_needed_dirs $name 'target' $dst_files $dst_desktop $dst_documents $dst_library
 
 print_backup_start $purpose $name $dada_hostname
 print_last_backup_time $name
-print_backup_dirs $src_files $dst_files $src_desktop $dst_desktop $src_documents $dst_documents
+print_backup_dirs $src_files $dst_files $src_desktop $dst_desktop $src_documents $dst_documents $src_library $dst_library
 
-copy_rsync_delete $src_files $dst_files 0 "VMs"
+copy_rsync_delete $src_files $dst_files 0 "VMs" "Music"
 copy_rsync_delete $src_desktop $dst_desktop 0
+copy_rsync_delete $src_documents $dst_documents 0
 copy_rsync_delete $src_documents $dst_documents 0
 
 print_backup_finish $name
