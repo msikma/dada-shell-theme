b36130d77a59e7a6dd3357bf71d06f60341efa1f
Michiel Sikma <michiel@sikma.org>
2020-04-05 18:38:22 +0200

    Add first correctly working version of ia_scans script

--- bin/ia_scans.sh
+++ bin/ia_scans.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 PROJECT="ia_imgs"
-DESCRIPTION="Prints an Internet Archive item's description with a table of scans."
+DESCRIPTION="Prints an Internet Archive item's description along with a table of scans."
 SELF="ia_imgs.sh"
 VERSION="1.0.0"
-PREREQUISITES=('ia')
+PREREQUISITES=('ia' 'jq')
 
 function check_prerequisites {
   for tool in "${PREREQUISITES[@]}"; do
@@ -15,14 +15,52 @@ function check_prerequisites {
   done
 }
 
+# Lists all original files whose title starts with 'Scan [0-9]'.
+function list_scans {
+  ia list -l --columns=name,source "$1" | grep "original\$" | grep -i "/scan.[0-9]\+" | cut -d $'\t' -f1 | sort | tr '\n' '\0'
+}
+
 function print_desc {
-  ia metadata "$1" | jq '.metadata.description'
+  ia metadata "$1" | jq -r '.metadata.description'
+}
+
+function print_item {
+  echo "<tr><td>$4</td><td><a href=\"$1\"><img src=\"$2\" alt=\"Thumbnail of $3\" style=\"display:block\"></a></td></tr>"
+}
+
+function print_items_start {
+  echo "<p><hr></p><p><strong>Scans:</strong></p><table border=\"1\"><tr><th>Name</th><th>Image</th></tr>"
+}
+
+function print_items_end {
+  echo "</table>"
 }
 
 function print_desc_with_images {
-  desc=$(print_desc "$1")
-  echo $desc
-  echo 'TODO'
+  # Create an array of URLs to scans, e.g. 'https://archive.org/download/IDENTIFIER/Scan 01 - Cover.jpg'.
+  files=()
+  while IFS= read -r -d $'\0'; do
+    files+=("$REPLY")
+  done < <(list_scans "$1")
+
+  # The original description.
+  print_desc "$1"
+
+  # Now we'll loop through each scan and output an HTML item for each one.
+  print_items_start
+  for file in "${files[@]}"; do
+    # The plain filename without URL path.
+    fn=$(echo $file | sed -n 's/.*\/download\/.*\/\(.*\)$/\1/p')
+    # The title of the scan (e.g. 'Cover' for 'Scan 01 - Cover.jpg')
+    name=$(echo $fn | sed -n 's/[^-]* - \(.*\)\..*/\1/p')
+    # Attach _thumb at the end to get the thumbnail URL.
+    thumb=$(echo $file | sed -e 's/.\([a-z]*\)$/_thumb.\1/')
+    print_item "$file" "$thumb" "$fn" "$name"
+  done
+  print_items_end
+  #
+  #echo $desc
+  #echo 'TODO'
 }
 
 function argparse {
