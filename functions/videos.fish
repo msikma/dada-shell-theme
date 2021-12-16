3c791f9fed73c32f1916b009f85a5cd914aa5e3d
Michiel Sikma <michiel@sikma.org>
2021-12-16 21:57:53 +0100

    Add x264_vg command

--- functions/videos.fish
+++ functions/videos.fish
@@ -32,3 +32,18 @@ function enc_x264_rgbll --argument-names src dst
   _notify_encode "$src" "$dst" "x264 rgb lossless (libx264rgb, crf 0; audio flac)"
   ffmpeg -y -i "$src" -threads 8 -c:a flac -c:v libx264rgb -preset slow -crf 0 -x264-params "keyint=300:ref=16:no-fast-pskip:bframes=16:merange=64:subme=9:trellis=2:partitions=all:no-dct-decimate" -stats "$dst"
 end
+
+function x264_vg --description "Encodes x264 for 2D video games (upscaling 4x)"
+  if [ (count $argv) -eq 0 ]
+    echo 'x264ll: usage: x264ll filename1.avi[, filenameN.avi[, ...]]'
+    return 1
+  end
+  for file in $argv[1..-1]
+    set base (_file_basename $file)
+    set ext (_file_extension $file)
+    set target $base'.m4v'
+
+    echo "Converting "(set_color yellow)"$file"(set_color reset)" to "(set_color green)"$target"(set_color reset)"."
+    ffmpeg -y -i "$file" -c:v libx264 -pix_fmt yuv444p -filter:v scale="iw*4:ih*4:flags=neighbor" -profile:v high444 -crf 20 -preset:v ultrafast -v quiet -stats "$target"
+  end
+end
