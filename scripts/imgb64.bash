756579e93a1e809f91b050beb6baae56cda33ac5
Michiel Sikma <michiel@sikma.org>
2023-05-31 14:37:12 +0200

    Add imgb64 alias and fix bug when image file does not exist

--- scripts/imgb64.bash
+++ scripts/imgb64.bash
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if ! test -f "$1"; then
+  echo "imgb64: error: file \"$1\" does not exist"
+  exit 1
+fi
+
 # Retrieves the mime type, e.g. 'image/png'
 mime=$(file -bN --mime-type "$1")
 # Converts the file itself to Base64
